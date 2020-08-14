5996dad2fcbecaa86646c9d466af18677fd9336a
Yash Jagarwal <61383487+jagrawalyash@users.noreply.github.com>
2020-08-14 23:21:14 +0530

    Initial Commit

---
+++ build/precache-manifest.1078953fecf9dd8440717466e17e90f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569b98a655f71691e7445824b64631d3",
+    "url": "/taskreact/index.html"
+  },
+  {
+    "revision": "03264ef7382665a4d3f1",
+    "url": "/taskreact/static/css/main.f78e405e.chunk.css"
+  },
+  {
+    "revision": "a6127f3d00f992f5a22e",
+    "url": "/taskreact/static/js/2.93a5bcbc.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/taskreact/static/js/2.93a5bcbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03264ef7382665a4d3f1",
+    "url": "/taskreact/static/js/main.e9c19d36.chunk.js"
+  },
+  {
+    "revision": "490bdb4caa0b1ebb7834",
+    "url": "/taskreact/static/js/runtime-main.876ff27e.js"
+  }
+]);
